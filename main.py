e71634226e50528ad4711619c15025a5311f97bc
Michelle <michellehnguyen03@gmail.com>
2021-07-04 17:43:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+
+    def my_mad_libs():
+        noun = input("Please type in a noun.\n")
+        adj = input("Please type in a adjective.\n")
+        verb = input("Please type in a verb.\n")
+
+        print(f"Hi, I am a {noun} and I am {adj}. My favorite thing to do is {verb}!")
+    my_mad_libs()
+
+if __name__ == '__main__':
+    main()
